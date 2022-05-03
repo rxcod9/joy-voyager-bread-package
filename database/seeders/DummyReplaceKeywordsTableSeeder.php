02513117c4b425fa699ca2e454dbd18326035421
Ramakant <gangwar.ramakant@gmail.com>
2022-05-03 19:24:06 +0530

    💚_Seeder-count

--- database/seeders/DummyReplaceKeywordsTableSeeder.php
+++ database/seeders/DummyReplaceKeywordsTableSeeder.php
@@ -14,7 +14,7 @@ class DummyReplaceKeywordsTableSeeder extends Seeder
      */
     public function run()
     {
-        $count = 100;
+        $count = 20;
         ReplaceKeyword::factory()
             ->count($count)
             ->state(function (array $attributes) use ($count) {
